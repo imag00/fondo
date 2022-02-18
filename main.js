c31738999154e3b34beb48fb7879adf5a678735d
Ismael Magro <ismael.magro2000@gmail.com>
2022-02-18 22:47:41 +0100

    Icons

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener("click", function(evnt){
+    deselectAllIcons();
+    iconClick(evnt.target);
+});
+
+function iconClick(element) {
+    if (element.className != "icon") return;
+
+    element.style.backgroundColor = '#008cff17';
+    element.style.border = '2px solid #008cff2c';    
+}
+
+function deselectAllIcons() {
+    var icons = document.getElementsByClassName("icon");
+
+    for (var icon of icons) {
+        icon.style.backgroundColor = '';
+        icon.style.border = 'none';
+    }
+}
+
+function openYTLink(link) {
+    window.open('https://youtu.be/' + link);
+}
